05d3f1dbda5d4b1404b6cf59db764326925806af
tgprudencio <thiago.gprudencio@gmail.com>
2023-10-09 19:44:44 -0300

    feat: add login route and files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaView, StatusBar, Text, View } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { Login } from './screens/login/Login';
+
+const appTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    mainColor: '#FF8453',
+    background: '#F3F2ED',
+    backgroundSecondary: '#F8E0E4',
+    card: '#F3F2ED',
+    text: '#38424B',
+    textDisabled: '#959A9C',
+    border: '#38424B',
+    placeholder: '#38424B',
+    success: '#1CC88A',
+    successIcon: '#13855C',
+  },
+};
+
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView style = {{ backgroundColor: '#476969', flex: 1 }}>
-        <StatusBar barStyle='light-content' />
-        
-      </SafeAreaView>
+      
+      <StatusBar barStyle = 'light-content' />
+      <Stack.Navigator initialRouteName = "Login" screenOptions = {{ headerShown: false, headerLeft: null, animation: 'slide_from_right', contentStyle: { backgroundColor: '#476969' }}}>
+        <Stack.Screen name = "Login" component = { Login } />
+      </Stack.Navigator>
+      
     </NavigationContainer>
   );
 }
---
+++ src/screens/login/Login.js
@@ -0,0 +1,12 @@
+import  React, { useEffect, useState } from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+export function Login({ route, navigation }) {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Text>dsadasdadsads Screen</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
