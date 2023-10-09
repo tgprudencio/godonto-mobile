d51b92ae429f35f516af1fd678c894a9587beda2
tgprudencio <thiago.gprudencio@gmail.com>
2023-10-09 16:49:24 -0300

    feat: first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView style = {{ backgroundColor: '#476969', flex: 1 }}>
+      <StatusBar barStyle='light-content' />
+      
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
