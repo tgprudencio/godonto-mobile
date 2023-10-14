143c0cbf5b665e0daf367f0fd531b334a99d6a8d
tgprudencio <thiago.gprudencio@gmail.com>
2023-10-14 13:01:29 -0300

    fix: localhost ip for android emulator

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import { create } from 'apisauce';
 
 const api = create ({
-    baseURL: 'http://localhost:3333',
+    baseURL: 'http://192.168.1.14:3333',
     headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
 });
 
